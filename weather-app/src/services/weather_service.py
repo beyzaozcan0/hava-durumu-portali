cc9b82897ace599bfdcc69e8a38d8c9cbd415262
beyzaozcan0 <ozcanbeyza089@gmail.com>
2025-07-27 23:37:21 +0300

    bitti

--- weather-app/src/services/weather_service.py
+++ weather-app/src/services/weather_service.py
@@ -1,7 +1,7 @@
 class WeatherService:
     def fetch_weather(self, city):
         import requests
-        api_key = "64880a8a38d28b7b72924741c2de00ad"
+        api_key = "Kendi_API_Keyini_Gir"
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
         response = requests.get(url)
         if response.status_code == 200:
